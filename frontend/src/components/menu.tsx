71b3fca086e7016d929c13253a6be516d0c65da4
patrick9as <patrick9as@gmail.com>
2025-04-06 20:59:26 -0300

    refact: organização do código

--- frontend/src/components/menu.tsx
+++ frontend/src/components/menu.tsx
@@ -6,21 +6,26 @@ interface MenuProps {
   onLogout: () => void
 }
 
+const openPDV = () => {
+  api.get('/pdv/open')
+    .then(() => {
+      toast.success('Abrindo PDV');
+    }
+    )
+    .catch(() => {
+      toast.error('Erro ao abrir PDV');
+    });
+}
+
 export const Menu = ({onLogout}: MenuProps) => {
   return (
     <>
-      <div className="bg-[#16140C] w-full flex flex gap-1">
+      <div className="bg-[#16140C] w-full flex gap-1">
         <div className="p-4 pt-5 pb-3"><img src={Logo} className="w-[65px]"></img></div>
         <div className="flex mt-4 flex-row w-fit font-semibold bg-white text-black p-2 px-4 rounded-t-[12px]">Geral</div>
         <div className="flex mt-4 flex-row w-fit font-semibold bg-none text-white p-2 px-4 rounded-t-[12px]">Cadastro</div>
         <div className="flex mt-4 flex-row w-fit font-semibold bg-none text-white p-2 px-4 rounded-t-[12px]">Financeiro</div>
-        <div className="flex mt-4 flex-row w-fit font-semibold bg-none text-white p-2 px-4 rounded-t-[12px]" onClick={() => {
-              api.get('/pdv/open')
-                .then(() => {
-                  toast.success('Abrindo PDV');
-                }
-                )
-            }}>Vendas</div>
+        <div className="flex mt-4 flex-row w-fit font-semibold bg-none text-white p-2 px-4 rounded-t-[12px]" onClick={() => openPDV()}>Vendas</div>
         <div className="flex mt-4 flex-row w-fit font-semibold bg-none text-white p-2 px-4 rounded-t-[12px]">Estoque</div>
         <div className="flex mt-4 flex-row w-fit font-semibold bg-none text-white p-2 px-4 rounded-t-[12px]">Relatórios</div>
         <div className="flex mt-4 flex-row w-fit font-semibold bg-none text-white p-2 px-4 rounded-t-[12px]">Utilitários</div>
